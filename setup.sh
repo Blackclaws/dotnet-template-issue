caf111a87aae93a6693c111413eda4d70f19bb5d
Felix Winterhalter <felix.winterhalter@fau.de>
2021-07-19 15:57:22 +0200

    Minimal example

--- setup.sh
+++ setup.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-git init -b main
-rm setup.sh
-git add .
-git commit 
+echo "This line is visible and goes to stdout."
+>&2 echo "This line is hidden and goes to stderr"
+echo "This line is visible again, the one before it is missing however. Comment in the false at the end to print the missing line at the end. Press enter to continue"
+read
+#false
